a97760f150eb5295443153605e816532bf5156cf
linfengisep <wang.linfeng0213@gmail.com>
2018-05-19 12:43:25 +0200

    delete projet

--- database/src/main/java/com/example/database/Task.java
+++
@@ -1,9 +0,0 @@
-@Entity
-public class Task{
-    @PrimaryKey
-    public int taskId;
-    public String taskContent;
-    public int taskPriority;
-    public long taskCreationDate;
-    public long taskDeadLine;
-}
